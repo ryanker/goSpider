7fabb12c51f343162b2097cf40b900361570f838
Ryan <ryanker1024@gmail.com>
2019-03-25 12:15:36 +0800

    add lib dbs

---
+++ lib/dbs/dbs.go
@@ -0,0 +1,143 @@
+package dbs
+
+import (
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+	"strings"
+)
+
+type DB struct {
+	*sql.DB
+}
+
+type H map[string]interface{}
+
+func Open(dataSourceName string) (*DB, error) {
+	db, err := sql.Open("sqlite3", dataSourceName)
+	if err != nil {
+		return nil, err
+	}
+	return &DB{DB: db}, err
+}
+
+func (db *DB) Insert(table string, data H) (id int64, err error) {
+	kStr, vStr, args := GetSqlInsert(data)
+	stmt, err := db.Prepare("INSERT INTO `" + table + "`(" + kStr + ") VALUES (" + vStr + ")")
+	if err != nil {
+		return
+	}
+
+	res, err := stmt.Exec(args...)
+	if err != nil {
+		return
+	}
+
+	id, err = res.LastInsertId()
+	return
+}
+
+func (db *DB) Delete(table string, where H) (n int64, err error) {
+	whereStr, args := GetSqlWhere(where)
+	stmt, err := db.Prepare("DELETE FROM `" + table + "`" + whereStr)
+	if err != nil {
+		return
+	}
+
+	res, err := stmt.Exec(args...)
+	if err != nil {
+		return
+	}
+
+	n, err = res.RowsAffected()
+	return
+}
+
+func (db *DB) Update(table string, data H, where H) (n int64, err error) {
+	setStr, args := GetSqlUpdate(data)
+	whereStr, args2 := GetSqlWhere(where)
+	stmt, err := db.Prepare("UPDATE `" + table + "` SET " + setStr + whereStr)
+	if err != nil {
+		return
+	}
+
+	args = append(args, args2...)
+	res, err := stmt.Exec(args...)
+	if err != nil {
+		return
+	}
+
+	n, err = res.RowsAffected()
+	return
+}
+
+func (db *DB) Read(table string, fields string, scanArr []interface{}, where H) (err error) {
+	whereStr, args := GetSqlWhere(where)
+	stmt, err := db.Prepare("SELECT " + fields + " FROM `" + table + "`" + whereStr)
+	if err != nil {
+		return
+	}
+
+	err = stmt.QueryRow(args...).Scan(scanArr...)
+	return
+}
+
+func (db *DB) Count(table string, where H) (n int64, err error) {
+	whereStr, args := GetSqlWhere(where)
+	stmt, err := db.Prepare("SELECT COUNT(*) FROM `" + table + "`" + whereStr)
+	if err != nil {
+		return
+	}
+
+	err = stmt.QueryRow(args...).Scan(&n)
+	return
+}
+
+func (db *DB) Find(table string, fields string, where H, page int64, pageSize int64) (rows *sql.Rows, err error) {
+	rows, err = db.Query("SELECT " + fields + " FROM `" + table + "`")
+	if err != nil {
+		return
+	}
+	return
+}
+
+func GetSqlRead(dest H) (fStr string, scanArr []interface{}) {
+	for k, v := range dest {
+		fStr += "`" + k + "`, "
+		scanArr = append(scanArr, v)
+	}
+	fStr = strings.TrimSuffix(fStr, ", ")
+	return
+}
+
+func GetSqlInsert(data H) (kStr, vStr string, args []interface{}) {
+	for k, v := range data {
+		kStr += "`" + k + "`, "
+		vStr += "?, "
+		args = append(args, v)
+	}
+	kStr = strings.TrimSuffix(kStr, ", ")
+	vStr = strings.TrimSuffix(vStr, ", ")
+	return
+}
+
+func GetSqlUpdate(data H) (setStr string, args []interface{}) {
+	for k, v := range data {
+		setStr += "`" + k + "`=?, "
+		args = append(args, v)
+	}
+	setStr = strings.TrimSuffix(setStr, ", ")
+	return
+}
+
+func GetSqlWhere(where H) (whereStr string, args []interface{}) {
+	if len(where) == 0 {
+		return
+	}
+	whereStr = " WHERE "
+	for k, v := range where {
+		whereStr += "`" + k + "`=? AND "
+		args = append(args, v)
+	}
+	whereStr = strings.TrimSuffix(whereStr, " AND ")
+	return
+}
---
+++ lib/dbs/example/simple.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"../../dbs"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+	"os"
+	"time"
+)
+
+type User struct {
+	Uid        int64
+	Gid        int64
+	Name       string
+	CreateDate string
+}
+
+var err error
+var db *dbs.DB
+
+func main() {
+	os.Remove("./test.db")
+
+	db, err = dbs.Open("./test.db")
+	checkErr(err)
+	defer db.Close()
+
+	// 创建表
+	_, err = db.Exec(`DROP TABLE IF EXISTS user;
+CREATE TABLE user
+(
+  uid        INTEGER PRIMARY KEY AUTOINCREMENT,
+  gid        INTEGER NOT NULL DEFAULT '0',
+  name       TEXT             DEFAULT '',
+  createDate DATETIME         DEFAULT CURRENT_TIMESTAMP
+);`)
+	checkErr(err)
+
+	// 插入
+	uid, err := db.Insert("user", dbs.H{
+		"gid":        1,
+		"name":       "admin",
+		"createDate": time.Now().Format("2006-01-02 15:04:05"),
+	})
+	checkErr(err)
+	fmt.Println("Insert: ", uid)
+
+	uid, err = db.Insert("user", dbs.H{
+		"gid":        1,
+		"name":       "admin2",
+		"createDate": time.Now().Format("2006-01-02 15:04:05"),
+	})
+	checkErr(err)
+	fmt.Println("Insert: ", uid)
+
+	// 更新
+	n, err := db.Update("user", dbs.H{
+		"gid":  "2",
+		"name": "test",
+	}, dbs.H{
+		"uid": uid,
+	})
+	checkErr(err)
+	fmt.Println("Update: ", n)
+
+	// 统计数量
+	n, err = db.Count("user", dbs.H{})
+	checkErr(err)
+	fmt.Println("Count: ", n)
+
+	// ===========================================================================
+	// 映射结构体
+	scanF := func() (ptr *User, fields string, args *[]interface{}) {
+		row := User{}
+		fields, scanArr := dbs.GetSqlRead(dbs.H{
+			"uid":        &row.Uid,
+			"gid":        &row.Gid,
+			"name":       &row.Name,
+			"createDate": &row.CreateDate,
+		})
+		ptr = &row
+		args = &scanArr
+		return
+	}
+	data, fields, scanArr := scanF()
+
+	// 读取(到结构体)
+	err = db.Read("user", fields, *scanArr, dbs.H{
+		"uid": uid,
+	})
+	checkErr(err)
+	u := *data
+	fmt.Printf("Read: %+v\n", u)
+
+	// 读取多条(到结构体)
+	rows, err := db.Find("user", fields, dbs.H{}, 1, 20)
+	checkErr(err)
+	var list []User
+	for rows.Next() {
+		err = rows.Scan(*scanArr...)
+		checkErr(err)
+		list = append(list, *data)
+	}
+	fmt.Println("List: ", list)
+
+	// 删除
+	n, err = db.Delete("user", dbs.H{
+		"uid": uid,
+	})
+	checkErr(err)
+	fmt.Println("Delete: ", n)
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
